cb772cc14378b8b9b9aba9d5f0e2ccebce4f6808
maigre <thomas@37m.gr>
2025-03-02 09:47:20 +0100

    install script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ BASEPATH="$(dirname "$(readlink -f "$0")")"
 cd "$BASEPATH"
 
 # Dependencies
-# sudo apt-get install python3-opencv python3-numpy python3-protobuf python3-venv 
+# sudo apt-get install python3-opencv python3-numpy python3-protobuf python3-venv  
 
 # Venv (legacy mode, now using poetry)
 # python3 -m venv --system-site-packages venv
@@ -20,15 +20,40 @@ cd "$BASEPATH"
 # wget https://www.terabee.com/wp-content/uploads/2020/06/TerraBee-SDK-1.0.0-Linux.zip OR use ./Redist
 
 # RealSense SDK
+apt install -y automake libtool cmake libusb-1.0-0-dev python3-protobuf libtbb-dev clang
+git clone https://github.com/IntelRealSense/librealsense.git --depth 1
+cd librealsense
+./scripts/setup_udev_rules.sh
+udevadm control --reload-rules
+udevadm trigger
+
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+mkdir build && cd build
+cmake .. -DBUILD_EXAMPLES=false -DCMAKE_BUILD_TYPE=Release -DFORCE_LIBUVC=true -DOTHER_LIBS="-latomic"
+make -j4 && make install
+cmake .. -DBUILD_PYTHON_BINDINGS=bool:true -DPYTHON_EXECUTABLE=$(which python3)
+make -j4 && make install
+# Link pyrealsense2.so to python3 site-packages
+cd Release
+for libfile in pyrealsense2.cpython-*.so; do
+    ln -sf "$PWD/$libfile" /usr/lib/python3/dist-packages/pyrealsense2/
+done
+cd "$BASEPATH"
+unset CC
+unset CXX
 
 
 # Poetry
 poetry install
+poetry run pip install opencv-python-headless
 
 
-ln -sf "$BASEPATH/3dcam" /usr/local/bin/
-ln -sf "$BASEPATH/3dcam.service" /etc/systemd/system/
-systemctl daemon-reload
+
+
+# ln -sf "$BASEPATH/3dcam" /usr/local/bin/
+# ln -sf "$BASEPATH/3dcam.service" /etc/systemd/system/
+# systemctl daemon-reload
 
 echo "3dcam INSTALLED"
 echo
